41566999949b5950fbce9174c0e1f1432dc53854
doc-ock <yveltcl@gmail.com>
2020-10-16 17:17:53 -0400

    changed read in files

---
+++ snippets.py
@@ -0,0 +1,9 @@
+import numpy as np
+    # data = np.zeros((0, 2))
+    # There are 5 sets of measurements per position so 5 files must be read in.
+    # for j in range(testnum):
+    #    if i is None:
+    #        break
+    #    data = np.concatenate((data, np.genfromtxt(i, delimiter=',')), axis=0)
+    #    i = next(pos, None)
+    # data = data.reshape((testnum, int(np.size(data) / 10), -1))
